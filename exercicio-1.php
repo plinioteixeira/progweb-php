126ca66eb215b5ff0f447cc9826a83c915497936
plinioteixeira <plinio.t.vasconcellos@gmail.com>
2022-04-26 15:51:06 -0300

    exercício finalizado

---
+++ exercicio-1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício</title>
+</head>
+<body>
+    
+<?php 
+
+$data = "26/04/2022";
+$nome = "Robson";
+$idade = 35;
+$cidade = "São Paulo";
+
+
+?>
+<p>
+    hoje é <?=$data;?>. <?=$nome;?> tem <?=$idade;?> anos e nasceu em <?=$cidade?>
+</p>
+    
+</body>
+</html>
