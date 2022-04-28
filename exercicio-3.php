c3e23bb46cf2b468562179a0dc1d5c8244aa2763
plinioteixeira <plinio.t.vasconcellos@gmail.com>
2022-04-28 14:31:51 -0300

    Exercício 3 utilizando number_format

--- exercicio-3.php
+++ exercicio-3.php
@@ -12,19 +12,28 @@
 <?php 
     $salario = 600;
 
-    echo "<p>Total: $salario</p>"
+    echo "<p>Salário antigo: <span style='color:red; font-size: 25px'>R$$salario</span></p>"
 ?>
 
 <?php 
     if ($salario < 500) {
         $salarioNovo = $salario * 1.15;
     } elseif ($salario <= 1000) {
-     "<p>$salarioNovo</p>"*1.10;
+     $salarioNovo = $salario *1.10;
     } else  {
-     "<p>$salarioNovo</p>"*1.05;
+     $salarioNovo = $salario *1.05;
     }
-    echo "<p> $salarioNovo</p>";
+    
+    echo "<p>Salário reajustado: <span style='color:blue; font-size: 25px'>R$$salarioNovo</span></p>";
 ?>
+<!-- number_format (
+    valor,
+    quantidades de casas decimais,
+    "separador de casa decimal"
+    "separador de milhar"
+) -->
+<p>R$ <?=number_format($salario,2,",",".")?> e R$ <?=number_format($salarioNovo,2,",",".")?> </p>
+
     
 </body>
 </html>
