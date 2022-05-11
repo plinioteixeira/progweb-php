e2ef6a8c692d55d7e0515aac5093690346aa3b16
plinioteixeira <plinio.t.vasconcellos@gmail.com>
2022-05-11 13:08:34 -0300

    cursos da oneup

--- ex-formulario-html-com-php.php
+++ ex-formulario-html-com-php.php
@@ -9,10 +9,48 @@
 <body>
 
 celular
-samsung, penalty, Natura, Nestlé
+samsung, penalty, natura, nestlé
 valor de 100 até 10000 acrescentando centavos
 quantidade de produtos 
 descrever porque quer
+
+<h1>Formulário de cadastro</h1>
+<hr>
+
+<form action="">
+
+    <p>
+        <label for="nome">Nome:</label>
+        <input required type="text" name="nome" id="nome">
+    </p>
+
+    <div>
+        <p>Fabricante: <br></p>
+
+        <input type="radio" name="fabricante" id="samsung" value="samsung">
+            <label for="samsung">Samsung</label>
+
+        <input type="radio" name="fabricante" id="penalty" value="penalty">
+            <label for="penalty">Penalty</label>
+
+        <input type="radio" name="fabricante" id="natura" value="natura">
+            <label for="natura">Natura</label>
+
+        <input type="radio" name="fabricante" id="nestlé" value="nestlé">
+            <label for="nestlé">Nestlé</label>
+
+    </div>
+
+    <div>
+        
+    <p>
+            <label for="preco">R$ </label>
+            <input type="number" name="preco" id="preco" min="100" max="10000">
+            <option value="Preço"></option>
+        </p>
+    </div>
+
+</form>
     
 </body>
 </html>
